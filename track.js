a7b86dc02936420ede5e060bf1a4c49283b4b8f7
Neo <random.shu@gmail.com>
2026-07-28 00:05:39 +0800

    Improve track.js

--- track.js
+++ track.js
@@ -2,14 +2,16 @@ const Database = require("better-sqlite3");
 const db = new Database("motor.db");
 
 let target = "A2023CCC0717-7793901";
+let certi = "2023010717587788";
 
-function track (target) {
+// track by application number
+function trackAppl (target) {
     // return db.prepare(`SELECT * FROM correlation WHERE appl = ?`).get(target); // when nothing, return undefined
-    //! to be added: check target existed in appl first
+    // check target existed in appl first
     if (
         typeof target !== "string" || ! db.prepare(`SELECT appl FROM correlation WHERE appl = ?`).get(target)
     ) {
-        alert("Wrong!!");
+        console.error("Unknown application:", target);
         return;
     }
 
@@ -31,8 +33,44 @@ function track (target) {
         afwTarget = select.get(afwTarget).appl;
     }
 
+    // update certi: Fill in the missing certificate number for the original application
+    if (result.length > 1) {
+        let certificate = db.prepare(`SELECT certi FROM correlation 
+            WHERE appl = ?`).get(result[1]).certi;
+        
+        db.prepare(`UPDATE correlation 
+            SET certi = ? 
+            WHERE appl = ?`).run(certificate, result[0]);
+    }
 
-    return result;
+    return result; // all related applications array
 }
 
-console.log(track(target));
+// let applications = trackAppl(target);
+
+// if (applications) {
+//     let placeholders = applications.map(() => "?").join(", ");
+//     let show = db.prepare(`SELECT c.appl, a.report FROM correlation c
+//                 INNER JOIN appl_certi a ON c.appl = a.appl
+//                 WHERE c.appl IN (${placeholders})`);
+
+//     console.log(show.all(applications));
+// }
+
+// track by certificate
+function trackCert(cert) {
+    if (
+        typeof cert !== "string" || ! db.prepare(`SELECT certi FROM correlation WHERE certi = ?`).get(cert)
+    ) {
+        console.error("Unknown certificate:", cert);
+        return;
+    }
+
+    // from now on, count(cert) >= 1
+    let applNum = db.prepare(`SELECT appl FROM correlation WHERE certi = ?`).get(cert).appl;
+    trackAppl(applNum); // Just leverage filling in the missing certificate number for the original application
+    return db.prepare(`SELECT appl FROM correlation WHERE certi =? ORDER BY appl DESC`).all(cert);
+
+}
+
+console.log(trackCert(certi));
